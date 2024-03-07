885692d012fb010e6bf6f689250f7058a6b40da4
prince <princekrvert@gmail.com>
2024-03-07 16:12:23 +0530

    addting bomber

---
+++ bomber.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import time
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
+from pprint import pprint
+import random 
+# add the argparser to fully fuction the program...
+import argparser
+# note you need to import to the sib_api_v3_sdk to install this exeute the cmd pip install sib_api_v3_sdk or pip3 install sib_api_v3_sdk
+
+configuration = sib_api_v3_sdk.Configuration()
+configuration.api_key['api-key'] = "your api key here"
+
+api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+subject = random.choice(gali)
+html_content = "hi prince"
+sender = {"name":"Vishal Kadam","email":""}
+to = [{"email":"youremail.com","name":""}]
+cc = [{"email":"example2@example2.com","name":"pata nahi"}]
+bcc = [{"name":"sala","email":"example@example.com"}]
+reply_to = {"email":"replyto@domain.com","name":""}
+headers = {"Some-Custom-Name":"unique-id-1234"}
+params = {"parameter":"My param value","subject":random.choice(gali)}
+send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, bcc=bcc, cc=cc, reply_to=reply_to, headers=headers, html_content=html_content, sender=sender, subject=subject)
+
+try:
+    api_response = api_instance.send_transac_email(send_smtp_email)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
