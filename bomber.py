cc2e6f3a785fabc1985ad11631a43686cf007752
prince <princekrvert@gmail.com>
2024-03-08 15:18:08 +0530

    second update in tblast

--- bomber.py
+++ bomber.py
@@ -5,14 +5,24 @@ from sib_api_v3_sdk.rest import ApiException
 from pprint import pprint
 import random 
 # add the argparser to fully fuction the program...
-import argparser
+import argparse
+parser = argparse.ArgumentParser(description='Mail spoofer and bomber')
+parser.add_argument("-f","--file",help='Enter the file path if you want')
+parser.add_argument('-n',"--number",help="Enter the amout how may time you want to send the mail",default=1 )
+args = parser.parse_args()
 # note you need to import to the sib_api_v3_sdk to install this exeute the cmd pip install sib_api_v3_sdk or pip3 install sib_api_v3_sdk
-
+ 
 configuration = sib_api_v3_sdk.Configuration()
 configuration.api_key['api-key'] = "your api key here"
 
 api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
-subject = random.choice(gali)
+s_mail =input("\033[33;1m Enter the subject: ")
+subject = s_mail
+# now ask for the actual msg 
+# check if file name is empty 
+if args.file:
+    print("file name present")
+msg_mail = input("\033[31;1m Enter msg to send")
 html_content = "hi prince"
 sender = {"name":"Vishal Kadam","email":""}
 to = [{"email":"youremail.com","name":""}]
@@ -20,7 +30,7 @@ cc = [{"email":"example2@example2.com","name":"pata nahi"}]
 bcc = [{"name":"sala","email":"example@example.com"}]
 reply_to = {"email":"replyto@domain.com","name":""}
 headers = {"Some-Custom-Name":"unique-id-1234"}
-params = {"parameter":"My param value","subject":random.choice(gali)}
+params = {"parameter":"My param value","subject":subject}
 send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, bcc=bcc, cc=cc, reply_to=reply_to, headers=headers, html_content=html_content, sender=sender, subject=subject)
 
 try:
